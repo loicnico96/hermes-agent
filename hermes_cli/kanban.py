9865d874fa3fb0d49a0d569fb3848666d0bd4e77
Loïc Nicolas <loicnico96@gmail.com>
2026-05-27 08:43:14 +0000

    refactor(kanban): trim redundant approvals imports

--- hermes_cli/kanban.py
+++ hermes_cli/kanban.py
@@ -1461,7 +1461,6 @@ def _cmd_show(args: argparse.Namespace) -> int:
             file=sys.stderr,
         )
         return 2
-    from hermes_cli import kanban_approvals_db as approvals_db
 
     with kb.connect_closing() as conn:
         task = kb.get_task(conn, args.task_id)
